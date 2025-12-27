7fa0bc91a40e85d21d35effecb3b42c7ee40881b
Ayoub <123956897+Jounaidayoub@users.noreply.github.com>
2025-12-27 22:46:18 +0100

    test the d1 database bindings

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,6 +1,22 @@
 import { Hono } from "hono";
+
 const app = new Hono<{ Bindings: Env }>();
 
-app.get("/api/", (c) => c.json({ name: "Cloudflare and this is deployed perfectly" }));
+app.get("/api/", async (c) => {
+  const result = await c.env.d1_cfw.prepare("SELECT * FROM Customers").run();
+  console.log(result);
+  const data = result.results;
+  const Customers = data.map((item) => item.ContactName).join(", ");
+  return c.json({
+    name: `Cloudflare and this is deployed perfectly and custumors are :
+    
+    
+  \n\n
+    
+    
+    ${Customers}`,
+  });
+});
+
 
 export default app;
