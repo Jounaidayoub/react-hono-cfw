015f5d0b3af4cce2a83ce250aee67c59e8158c0b
Ayoub <123956897+Jounaidayoub@users.noreply.github.com>
2026-01-01 03:44:13 +0100

    Revert "feat: Implement custom password hashing with `node:crypto` for `better-auth` and update worker compatibility flags."
    This reverts commit d21cf97f9de8511593c310627e49be040fceb3f0.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,6 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./db"; // your drizzle instance
-import { scryptSync } from "node:crypto";
 
 import { env } from "cloudflare:workers";
 
@@ -11,48 +10,6 @@ export const auth = betterAuth({
   }),
   emailAndPassword: {
     enabled: true,
-    /// HACK : this just a workaround for now from 
-    // https://github.com/better-auth/better-auth/issues/969
-    // 
-    password: {
-      hash: async (password) => {
-        const salt = crypto.getRandomValues(new Uint8Array(16));
-        const saltHex = Array.from(salt).map(b => b.toString(16).padStart(2, '0')).join('');
-
-        const key = scryptSync(
-          password.normalize("NFKC"),
-          saltHex,
-          64,
-          {
-            N: 16384,
-            r: 16,
-            p: 1,
-            maxmem: 128 * 16384 * 16 * 2
-          }
-        );
-
-        const keyHex = Array.from(key).map(b => b.toString(16).padStart(2, '0')).join('');
-        return `${saltHex}:${keyHex}`;
-      },
-      verify: async ({ hash, password }) => {
-        const [saltHex, keyHex] = hash.split(":");
-
-        const targetKey = scryptSync(
-          password.normalize("NFKC"),
-          saltHex,
-          64,
-          {
-            N: 16384,
-            r: 16,
-            p: 1,
-            maxmem: 128 * 16384 * 16 * 2
-          }
-        );
-
-        const targetKeyHex = Array.from(targetKey).map(b => b.toString(16).padStart(2, '0')).join('');
-        return targetKeyHex === keyHex;
-      },
-    },
   },
   baseURL: env.BETTER_AUTH_URL,
   socialProviders: {
